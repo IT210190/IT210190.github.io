a1bfcb452e398794674a3ee3db83d5ed38646845
IT210190 <113981352+IT210190@users.noreply.github.com>
2022-11-24 13:08:18 +0100

    Update script.js

--- weather/script.js
+++ weather/script.js
@@ -12,7 +12,7 @@ function main() {
                 document.getElementById('erg').innerHTML += '<h2 class="date" onclick="changeToDetails(' + i + ')">' + date.toString().split(" ")[2] + '. ' + date.toString().split(" ")[1] + ' ' + date.toString().split(" ")[3] +'</h2>' +
                 '<div id="container' + i + '" class="container">' +
                 '<div class="littleContainer"><h3>Temperatur</h3><p style="color: yellow;">' + (erg.list[i].temp.day) + '°C</p></div>' +
-                '<div class="littleContainer"><h3>' + erg.list[i].weather[0].main + '</h3><img src="http://openweathermap.org/img/wn/' + erg.list[i].weather[0].icon + '@2x.png"></div>' +
+                '<div class="littleContainer"><h3>' + erg.list[i].weather[0].main + '</h3><img src="http://openweathermap.org/img/wn/' + erg.list[i].weather[0].icon + '@2x.png" alt="weather"></div>' +
                 '<div class="littleContainer"><h3>Feuchtigkeit</h3><p style="color: blue;">' + erg.list[i].humidity + '%</p></div>' +
                 '<div class="littleContainer"><h3>Wind</h3><p>' + erg.list[i].speed + ' M/S</p><p>' + erg.list[0].deg + '°</p></div>' +
                 '</div><hr>';
